3f5ada219b39000d29f94d211aa3c85fea76eaf3
daniel <danielsprado12@gmail.com>
2021-02-23 14:52:43 -0300

    Textos

--- src/App.vue
+++ src/App.vue
@@ -19,3 +19,8 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+
+</style>
--- src/site/home/Research.vue
+++ src/site/home/Research.vue
@@ -1,6 +1,55 @@
 <template>
     <div class="home">
-        Pesquisa
+        <div class="container">
+            <v-row>
+                <v-col cols="8">
+                    <span class="title">
+                        {{ $t('home_page.research.title') }}
+                    </span><br>
+                    <span class="white--text">
+                        {{ $t('home_page.research.subtitle') }}
+                    </span>
+                </v-col>
+                <v-col cols="4" class="pt-8">
+                    <v-btn rounded outlined class="button" @click="form=!form"><b> {{ $t('home_page.research.button') }} </b></v-btn>
+                </v-col>
+            </v-row>
+            <v-expand-transition>
+                <v-row v-if="form">
+                    <v-form v-model="valid">
+                        <v-container>
+                        <v-row>
+                            <v-col md="4" xl="4">
+                                <v-text-field
+                                    class="textfield"
+                                    solo
+                                    rounded
+                                    v-model="firstname"
+                                    label="Nome"
+                                />
+                            </v-col>
+                            <v-col md="4" xl="4">
+                                <v-text-field
+                                    solo
+                                    rounded
+                                    v-model="firstname"
+                                    label="E-mail"
+                                />
+                            </v-col>
+                            <v-col md="4" xl="4">
+                                <v-text-field
+                                    solo
+                                    rounded
+                                    v-model="firstname"
+                                    label="Nascimento"
+                                />
+                            </v-col>
+                        </v-row>
+                        </v-container>
+                    </v-form>
+                </v-row>
+            </v-expand-transition>
+        </div>
     </div>
 </template>
 
@@ -8,8 +57,40 @@
 export default {
     data(){
         return {
-
+            subtext: "Não se preocupe! Nossa pesquisa é totalmente anônima",
+            form: false,
+        }
+    },
+    methods: {
+        open_form(){
+            this.form = true
         }
     }
 }
 </script>
+
+<style scoped>
+.home{
+    background-color: #FFC79F;
+    padding-left: 10%;
+    padding-right: 10%;
+    font-family: 'Poppins', sans-serif;
+    text-align: center;
+    box-shadow: 0px 5px 5px rgba(128, 128, 128, 0.52);
+
+}
+.container{
+    padding-top: 30px;
+    padding-bottom: 30px;
+}
+.title{
+    color: #853A94;
+}
+.button{
+    color: #853A94;
+    background-color: white;
+}
+.textfield{
+    color: #853A94;
+}
+</style>
