84f7aab3b511f0f15316f5ddaa1ff7b4351cab30
VieiraHeitor <44251243+VieiraHeitor@users.noreply.github.com>
2021-03-08 18:12:47 -0300

    feat/div-quem-somos-done

--- src/site/home/We.vue
+++ src/site/home/We.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="home">
-        Quem somos
+        <v-container class="container text-center">
+            <p class="title">{{$t('home_page.we.title') }}</p>
+            <div class="content">
+            <p class="text-center">
+               {{$t('home_page.we.description') }}
+            </p>
+            </div>
+            <v-btn outlined class="button">{{$t('home_page.we.button') }}</v-btn>
+        </v-container>
+        
     </div>
 </template>
 
@@ -13,3 +22,39 @@ export default {
     }
 }
 </script>
+<style scoped>
+.home{
+    background-color: #FFFFFF;
+    padding-left: 10%;
+    padding-right: 10%;
+    font-family: 'Poppins', sans-serif;
+    box-shadow: 0px 5px 5px rgba(128, 128, 128, 0.52);
+
+}
+.container{
+    display: flexbox;
+    padding-top: 30px;
+    padding-bottom: 30px;
+    justify-content: center;
+    
+}
+.title{
+    color: #000000;
+    font-weight: 600;
+    padding-bottom: 10px;
+}
+.content{
+    font-weight: 500;
+
+    margin: auto;
+    max-width: 60%;
+    height: fit-content;
+}
+.button{
+    color: #853A94;
+    font-weight: 700;
+    background-color: white;
+    border-radius: 30px;
+}
+
+</style>
--- src/texts/english/home.js
+++ src/texts/english/home.js
@@ -12,7 +12,12 @@ const home = {
         subtitle: "Don't worry! Our survey is completely anonymous",
         button: "Click Here"
     },
-    title: "Gloria Institute"
+    title: "Gloria Institute",
+    we:{
+        title:"Who is Gloria?",
+        description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus neque. Cras sit amet ligula ut justo commodo porta id ut enim.Nulla est lectus, mollis sit amet vehicula id, volutpat eget mauris.",
+        button: "Find out more"
+    }
 }
 
 export default home;
