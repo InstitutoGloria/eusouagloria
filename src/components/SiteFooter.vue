f6b89fced51682584e8349f035867f35287923d3
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-04-06 09:15:43 -0300

    Update SiteFooter.vue

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,18 +2,18 @@
     <div class="footer">
         <v-divider/>
         <v-row class="content">
-            <v-col cols=4>
-                <div class="pl-16 pb-5 pt-6">
+            <v-col class="pl-2 pr-2 pt-7" cols=4>
+                <div class="d-flex flex-column justify-space-between align-center">
                     <v-img width=130px contain src="../assets/images/home/gloria.png"></v-img>
                 </div>
             </v-col>
             <v-col cols=4>
-                <v-icon class="pl-2 pr-2 pt-7">mdi-twitter</v-icon>
-                <v-icon class="pl-2 pr-2 pt-7">mdi-facebook</v-icon>
-                <v-icon class="pl-2 pr-2 pt-7">mdi-instagram</v-icon>
+                <v-icon class="pl-2 pr-2 pt-6">mdi-twitter</v-icon>
+                <v-icon class="pl-2 pr-2 pt-6">mdi-facebook</v-icon>
+                <v-icon class="pl-2 pr-2 pt-6">mdi-instagram</v-icon>
             </v-col>
             <v-col cols=4>
-                <div class="text-caption pt-8 copyright">Copyright {{ new Date().getFullYear() }}</div>
+                <div class="text-caption pt-7 copyright">Copyright {{ new Date().getFullYear() }}</div>
             </v-col>
         </v-row>
     </div>
