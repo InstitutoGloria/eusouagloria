055aa991e12e154b82dbdd78f22a07efc5601467
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-05-07 17:07:20 -0300

    re-size image

--- src/site/home/Home.vue
+++ src/site/home/Home.vue
@@ -6,7 +6,7 @@
             <v-btn large :class="{'button-home-small': $vuetify.breakpoint. smAndDown, 'button-home': $vuetify.breakpoint. mdAndUp}"> {{$t('home_page.home.button') }}</v-btn>
         </div>
         <div :class="{'gloriaimg-small': $vuetify.breakpoint. smAndDown, 'gloriaimg': $vuetify.breakpoint. mdAndUp}">
-            <v-img height="50vh" contain src="../../assets/images/home/gloria-imagem-removebg-preview.png"/>
+            <v-img height="45vh" contain src="../../assets/images/home/gloria-imagem-removebg-preview.png"/>
         </div>
     </div>
    
