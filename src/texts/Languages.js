d315d1dc7fa1881845ab88777cfa737cb7ca3969
dasprado12 <danielsprado12@gmail.com>
2021-07-19 16:56:53 -0300

    including texts to i18n

--- src/texts/Languages.js
+++ src/texts/Languages.js
@@ -7,16 +7,21 @@ import we_pt from "./portugues/we.js"
 import news_en from './english/news.js'
 import news_pt from './portugues/news.js'
 
+import projects_en from './english/projects.js'
+import projects_pt from './portugues/projects.js'
+
 const Languages = {
     English: {
         home_page: home_en,
         we_page: we_en,
         news_page: news_en,
+        projects_page: projects_en,
     },
     Portugues: {
         home_page: home_pt,
         we_page: we_pt,
         news_page: news_pt,
+        projects_page: projects_pt,
     },
 }
 
