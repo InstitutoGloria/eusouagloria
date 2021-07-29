c324c656acb721fa251f5c803a36166f33007701
dasprado12 <danielsprado12@gmail.com>
2021-07-29 19:21:05 -0300

    Fixing texts i18n

--- src/texts/portugues/about.js
+++ src/texts/portugues/about.js
@@ -2,14 +2,21 @@
 // about > nome_component > nome_texto > "Texto"
 
 const about = {
-    
-    projects:{
-        button: "CONHEÇA NOSSOS PROJETOS",
+    golden_circle: {
+        title1: "A plataforma",
+        title2: "O CÍRCULO DE OURO",
+        first: "1 - A glória nasceu com o ideal de reduzir (e, por que não, combater) a opressão e violência física, psicológica e sexual contra mulheres",
+        second: "2 - A partir da coleta de dados, geramos conteúdo educacional e criamos uma rede de apoio com as ferramentas necessárias para a melhoria social",
+        third: "3 - Uma plataforma segura e de fácil acesso para mulheres em situação de vulnerabilidade e repressão"
     },
-    manifesto:{
-        title: "Conheça o nosso manifesto!",
+    manifesto: {
+        title: "Veja nosso manifesto!",
         text: "Loren ipsum dolor sit amet, consectetur adipiscing elit. sed at risus neque. cras sit emet ligula ut justo",
     },
+    goals: {
+        title: "Em 2 anos, queremos que a Glória esteja no mundo inteiro",
+        text: "O objetivo do projeto é impactar mais de 20 milhões de pessoas, além de gerar relatórios com segmentação por faixa etária, local, dados socioeconômicos e padrão de ocorrências, que auxiliem o público na formação de políticas, projetos e ações para combater a violência contra a mulher"
+    }
 
 }
 
