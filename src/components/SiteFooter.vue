b2a515bd3e1066f724a4cbac85a20debe3104484
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-08-06 21:34:17 -0300

    Update SiteFooter.vue

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -8,9 +8,15 @@
                 </div>
             </v-col>
             <v-col cols=4>
-                <v-icon class="pl-2 pr-2 pt-6">mdi-twitter</v-icon>
-                <v-icon class="pl-2 pr-2 pt-6">mdi-facebook</v-icon>
-                <v-icon class="pl-2 pr-2 pt-6">mdi-instagram</v-icon>
+                <v-btn icon href="https://twitter.com/eusouagloria" target="_blank">
+                    <v-icon class="pl-2 pr-2 pt-6">mdi-twitter</v-icon>
+                </v-btn>
+                <v-btn icon href="https://www.facebook.com/eusouagloria/" target="_blank">
+                    <v-icon class="pl-2 pr-2 pt-6">mdi-facebook</v-icon>
+                </v-btn>
+                <v-btn icon href="https://www.instagram.com/eusouagloria" target="_blank">
+                    <v-icon class="pl-2 pr-2 pt-6">mdi-instagram</v-icon>
+                </v-btn>          
             </v-col>
             <v-col cols=4>
                 <div class="text-caption pt-7 copyright">Copyright {{ new Date().getFullYear() }}</div>
